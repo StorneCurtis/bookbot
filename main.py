e3b1bed0cb4d4550127695595988ceeaf044f100
StorneCurtis <c.j.curtis101@gmail.com>
2025-08-18 21:49:13 -0600

    after report assignment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from stats import count_words
-from stats import count_characters
+from stats import count_words, count_characters, sort_dictionary
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,9 +6,20 @@ def get_book_text(path_to_file):
         print(file_contents)
 
 def main():
-    print(count_words("books/frankenstein.txt"))
-    print(count_characters("books/frankenstein.txt"))
-
-#    get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    word_count = count_words(book_path)
+    character_count = count_characters(book_path)
+    sorted_characters = sort_dictionary(count_characters(book_path))
+    #print(word_count)
+    #print(character_count)
+    #print(sorted_characters)
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {book_path}
+----------- Word Count ----------
+Found {word_count} total words
+--------- Character Count -------""")
+    for c in sorted_characters:
+        print(f"{c["char"]}: {c["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,17 +1,34 @@
-from collections import Counter
-
 def count_words(path_to_file):
     file_length = 0
     with open(path_to_file) as f:
         file_Contents = f.read()
         file_words = file_Contents.split()
         file_length = len(file_words)
-        return (f"{file_length} words found in the document")
-
+        return (len(file_words))
+    
 def count_characters(path_to_file):
+    char_dict = {}
     with open(path_to_file) as f:
-        file_contents = f.read().lower()
-        total_characters = len(file_contents)
-        cntr = Counter(file_contents)
-        return (cntr)
+        file = f.read().lower()
+        for c in file:
+            if c in char_dict:
+                char_dict[c] += 1
+            else:
+                char_dict[c] = 1
+        return char_dict
+    
+def sort_on(items):
+    return items["num"]
 
+def sort_dictionary(dict):
+    characters = []
+    for c in dict:
+        if c.isalpha():
+            temp_dict = {}
+            temp_dict["char"] = c
+            temp_dict["num"] = dict[c]
+            characters.append(temp_dict)
+    characters.sort(reverse=True,key=sort_on)
+    
+    #print(characters)
+    return characters
